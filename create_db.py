c47391001bf5ca1d972d8abb6736d51ade4e615e
koka737 <adinaoros31@gmail.com>
2025-03-13 11:05:28 +0100

    second commit

--- create_db.py
+++ create_db.py
@@ -16,14 +16,17 @@ def load(fname=JSONFILENAME, db_name=DBFILENAME):
   db_run('DROP TABLE IF EXISTS recipe')
   db_run('DROP TABLE IF EXISTS ingredient')
   db_run('DROP TABLE IF EXISTS stage')
+  db_run('DROP TABLE IF EXISTS user')
 
   db_run('CREATE TABLE recipe (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT, img TEXT, description TEXT, duration TEXT)')
   db_run('CREATE TABLE ingredient (recipe INT, rank INT, name TEXT)')
   db_run('CREATE TABLE stage (recipe INT, rank INT, description TEXT)')
+  db_run('CREATE TABLE user (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, password_hash TEXT)')
 
   insert1 = 'INSERT INTO recipe VALUES (:id, :title, :img, :description, :duration)'
   insert2 = 'INSERT INTO ingredient VALUES (:recipe, :rank, :name)'
   insert3 = 'INSERT INTO stage VALUES (:recipe, :rank, :description)'
+
 
   with open('recipes.json', 'r') as fh:
     recipes = json.load(fh)
@@ -34,6 +37,7 @@ def load(fname=JSONFILENAME, db_name=DBFILENAME):
       db_run(insert2, {'recipe': id, 'rank': r, 'name': ingredient['name']})
     for r, stage in enumerate(recipe['stages']):
       db_run(insert3, {'recipe': id, 'rank': r, 'description': stage['description']})
+    
 
 
 # load recipe data
