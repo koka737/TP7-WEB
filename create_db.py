4dd8a7ea9288cd7f6419e0d78c2fc068a58d4230
koka737 <adinaoros31@gmail.com>
2025-03-19 21:55:06 +0100

    almost finished

--- create_db.py
+++ create_db.py
@@ -1,45 +1,94 @@
 import sqlite3
 import json
+from werkzeug.security import generate_password_hash, check_password_hash
 
 
-JSONFILENAME = 'recipes.json'
-DBFILENAME = 'recipes.sqlite'
+JSONFILENAME = "recipes.json"
+DBFILENAME = "recipes.sqlite"
+
+
+def login(name, password):
+    with sqlite3.connect(DBFILENAME) as conn:
+        cur = conn.cursor()
+        cur.execute("SELECT id, password_hash FROM user WHERE name = ?", (name,))
+        user = cur.fetchone()
+
+        if user is None:
+            return -1  
+
+        user_id, password_hash = user
+
+        if check_password_hash(password_hash, password):
+            return user_id  # Login successful
+        else:
+            return -1 
+    
+def new_user(name, password):
+    password_hash = generate_password_hash(password)
+    with sqlite3.connect(DBFILENAME) as conn:
+        cur = conn.cursor()
+        cur.execute("INSERT INTO user (name, password_hash) VALUES (?, ?)", (name, password_hash))
+        conn.commit()
+
+        user_id = cur.lastrowid  
+    return user_id   
+        
+def create_user_table():
+    db_run(
+        "CREATE TABLE IF NOT EXISTS user (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, password_hash TEXT)"
+    )
+
+
+def add_test_user():
+    username = "testuser"
+    password = "hello123"
+
+    password_hash = generate_password_hash(password)
+    db_run(
+        "INSERT INTO user (name, password_hash) VALUES (?, ?)",
+        (username, password_hash),
+    )
+
 
 # Utility function
 def db_run(query, args=(), db_name=DBFILENAME):
-  with sqlite3.connect(db_name) as conn:
-    cur = conn.execute(query, args)
-    conn.commit()
+    with sqlite3.connect(db_name) as conn:
+        cur = conn.execute(query, args)
+        conn.commit()
+
 
 def load(fname=JSONFILENAME, db_name=DBFILENAME):
-  # possible improvement: do whole thing as a single transaction
-  db_run('DROP TABLE IF EXISTS recipe')
-  db_run('DROP TABLE IF EXISTS ingredient')
-  db_run('DROP TABLE IF EXISTS stage')
-  db_run('DROP TABLE IF EXISTS user')
+    # possible improvement: do whole thing as a single transaction
+    db_run("DROP TABLE IF EXISTS recipe")
+    db_run("DROP TABLE IF EXISTS ingredient")
+    db_run("DROP TABLE IF EXISTS stage")
+    db_run("DROP TABLE IF EXISTS user")
 
-  db_run('CREATE TABLE recipe (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT, img TEXT, description TEXT, duration TEXT)')
-  db_run('CREATE TABLE ingredient (recipe INT, rank INT, name TEXT)')
-  db_run('CREATE TABLE stage (recipe INT, rank INT, description TEXT)')
-  db_run('CREATE TABLE user (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, password_hash TEXT)')
+    db_run(
+        "CREATE TABLE recipe (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT, img TEXT, description TEXT, duration TEXT)"
+    )
+    db_run("CREATE TABLE ingredient (recipe INT, rank INT, name TEXT)")
+    db_run("CREATE TABLE stage (recipe INT, rank INT, description TEXT)")
+    create_user_table()
 
-  insert1 = 'INSERT INTO recipe VALUES (:id, :title, :img, :description, :duration)'
-  insert2 = 'INSERT INTO ingredient VALUES (:recipe, :rank, :name)'
-  insert3 = 'INSERT INTO stage VALUES (:recipe, :rank, :description)'
+    insert1 = "INSERT INTO recipe VALUES (:id, :title, :img, :description, :duration)"
+    insert2 = "INSERT INTO ingredient VALUES (:recipe, :rank, :name)"
+    insert3 = "INSERT INTO stage VALUES (:recipe, :rank, :description)"
 
+    with open("recipes.json", "r") as fh:
+        recipes = json.load(fh)
+    for id, recipe in enumerate(recipes):
+        recipe["id"] = id
+        db_run(insert1, recipe)
+        for r, ingredient in enumerate(recipe["ingredients"]):
+            db_run(insert2, {"recipe": id, "rank": r, "name": ingredient["name"]})
+        for r, stage in enumerate(recipe["stages"]):
+            db_run(
+                insert3, {"recipe": id, "rank": r, "description": stage["description"]}
+            )
 
-  with open('recipes.json', 'r') as fh:
-    recipes = json.load(fh)
-  for id, recipe in enumerate(recipes):
-    recipe['id'] = id
-    db_run(insert1, recipe)
-    for r, ingredient in enumerate(recipe['ingredients']):
-      db_run(insert2, {'recipe': id, 'rank': r, 'name': ingredient['name']})
-    for r, stage in enumerate(recipe['stages']):
-      db_run(insert3, {'recipe': id, 'rank': r, 'description': stage['description']})
-    
+        add_test_user()
 
 
 # load recipe data
 load()
-
--- server.py
+++ server.py
@@ -1,54 +1,141 @@
 from flask import Flask, session, Response, request, redirect, url_for, render_template
 import data_model as model
+from werkzeug.security import generate_password_hash, check_password_hash
+from functools import wraps
+
+global user_data
+username = "testuser"
+password = "hello123"
+password_hash = generate_password_hash(password)
+user_data = {"User": {"views": 0, "password_hash": password_hash}}
+
 
 app = Flask(__name__)
+
+
+app.secret_key = b"ec68a703c4a06b435b5654c34da1d8c6ae35ffcf060c7d70a6ab4f9cec2f2025"
 
 
 ########################################
 # Routes des pages principales du site #
 ########################################
 
+
+def login_required(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if not ("username" in session):
+            return Response("Acces Denied, you have to login", status=401)
+        else:
+            return f(*args, **kwargs)
+
+    return decorated_function
+
+
+@app.get("/login")
+def login_form():
+    return render_template("login.html")
+
+
+@app.post("/login")
+def verify_login():
+    username = request.form["username"]
+    password = request.form["password"]
+    error = None
+    if not (username in user_data):
+        error = "Nom d'utilisateur incorrect"
+    else:
+        password_hash = user_data[username]["password_hash"]
+        if not (check_password_hash(password_hash, password)):
+            error = "Mot de passe incorrect"
+    if error is None:
+        session.clear()
+        session["username"] = username
+        return redirect(url_for("home"))
+    else:
+        return redirect(url_for("login_form"))
+
+
+@app.post("/logout")
+def logout():
+    session.clear()
+    return redirect(url_for("home"))
+
+
 # Retourne une page principale avec le nombre de recettes
-@app.get('/')
+@app.get("/")
 def home():
-  return render_template('index.html')
+    if "username" in session:
+        username = session["username"]
+        user_data[username]["views"] = user_data[username]["views"] + 1
+        return render_template(
+            "index.html",
+            logged_in=True,
+            username=username,
+            views=user_data[username]["views"],
+        )
+    else:
+        return render_template("index.html", logged_in=False, username=None, views=None)
+
+
+@app.get("/new_user")
+def new_user_form():
+    return render_template("new_user.html")
+
+
+@app.post("/new_user")
+def add_new_user():
+    username = request.form["username"]
+    password = request.form["password"]
+    password_hash = generate_password_hash(password)
+
+    user_id = model.new_user(name, password)
+
+    session.clear()
+    session["username"] = name
+    return redirect(url_for("home"))
 
 
 # Retourne les résultats de la recherche à partir de la requête "query"
-@app.get('/search')
+@app.get("/search")
 def search():
-  if 'page' in request.args:
-    page = int(request.args["page"])
-  else:
-    page = 1
-  if 'query' in request.args:
-    query = request.args["query"]
-  else:
-    query = ""
-  found = model.search(query, page)
-  return render_template('search.html', found=found)
+    if "page" in request.args:
+        page = int(request.args["page"])
+    else:
+        page = 1
+    if "query" in request.args:
+        query = request.args["query"]
+    else:
+        query = ""
+    found = model.search(query, page)
+    return render_template("search.html", found=found)
+
 
 # Retourne le contenu d'une recette d'identifiant "id"
-@app.get('/read/<id>')
+@app.get("/read/<id>")
 def read(id):
-  recipe = model.read(int(id))
-  return render_template('read.html', recipe=recipe)
+    recipe = model.read(int(id))
+    return render_template("read.html", recipe=recipe)
 
 
-@app.get('/create')
+@app.get("/create")
+@login_required
 def create_form():
-  return render_template('create.html')
+    return render_template("create.html")
 
-@app.get('/update/<id>')
+
+@app.get("/update/<id>")
+@login_required
 def update_form(id):
-  recipe = model.read(int(id))
-  return render_template('update.html', recipe=recipe)
+    recipe = model.read(int(id))
+    return render_template("update.html", recipe=recipe)
 
 
-@app.get('/delete/<id>')
+@app.get("/delete/<id>")
+@login_required
 def delete_form(id):
-  entry = model.read(int(id))
-  return render_template('delete.html', id=id, title=entry['title'])
+    entry = model.read(int(id))
+    return render_template("delete.html", id=id, title=entry["title"])
 
 
 ############################################
@@ -57,37 +144,40 @@ def delete_form(id):
 
 
 def parse_user_list(user_list):
-  l = user_list.strip().split("-")
-  l = [e.strip() for e in l]
-  l = [e for e in l if len(e)> 0]
-  return l
+    l = user_list.strip().split("-")
+    l = [e.strip() for e in l]
+    l = [e for e in l if len(e) > 0]
+    return l
+
+
 # Fonction qui facilite la création d'une recette
 def post_data_to_recipe(form_data):
-  ingredients = parse_user_list(form_data['ingredients'])
-  stages = parse_user_list(form_data['stages'])
-  return {
-    'title': form_data['title'], 
-    'description': form_data['description'],
-    'img': form_data['img'],
-    'duration': form_data['duration'],
-    'ingredients': ingredients,
-    'stages': stages
-  }
+    ingredients = parse_user_list(form_data["ingredients"])
+    stages = parse_user_list(form_data["stages"])
+    return {
+        "title": form_data["title"],
+        "description": form_data["description"],
+        "img": form_data["img"],
+        "duration": form_data["duration"],
+        "ingredients": ingredients,
+        "stages": stages,
+    }
 
-@app.post('/create')
+
+@app.post("/create")
 def create_post():
-  id = model.create(post_data_to_recipe(request.form))
-  return redirect(url_for('read', id=str(id)))
+    id = model.create(post_data_to_recipe(request.form))
+    return redirect(url_for("read", id=str(id)))
 
 
-@app.post('/update/<id>')
+@app.post("/update/<id>")
 def update_post(id):
-  id = int(id)
-  model.update(id, post_data_to_recipe(request.form))
-  return redirect(url_for('read', id=str(id)))
+    id = int(id)
+    model.update(id, post_data_to_recipe(request.form))
+    return redirect(url_for("read", id=str(id)))
 
 
-@app.post('/delete/<id>')
+@app.post("/delete/<id>")
 def delete_post(id):
-  model.delete(int(id))
-  return redirect(url_for('home'))
+    model.delete(int(id))
+    return redirect(url_for("home"))
